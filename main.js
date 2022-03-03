961c3b40830104020a65f5cb8b08a407c830d664
Moritz Spranger <moritz.spranger@gmail.com>
2022-03-03 20:41:07 +0100

    Add slider code

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("Hi I'm an external js file NEW VERSION");
+
+//slider code
+
+var headings = ['erweitert Ihre bestehende IT-Landschaft', 'macht Vorgangsmanagement intuitiv', 'standardisiert Ihre Kunden&shy;kommunikation', 'kann sogar hybrid', 'verarbeitet ihre Daten und erstellt Reportings'];
+
+var headingElement = document.getElementById("dependent-header-span");
+
+var sliderElement = document.querySelector('#our-custom-slider .swiper-container');
+
+setTimeout( function(){ 
+        var sliderInstance = sliderElement.swiper;
+        sliderInstance.on('slideChange', function () {
+            let index = sliderInstance.activeIndex % 5;
+            headingElement.innerHTML = headings[index];
+        });
+        
+        // on first page load
+        let index = sliderInstance.activeIndex - 4;
+        headingElement.innerHTML = headings[index];
+}  , 5000 );
